2b43f69e8b7586388e8431a0b3141dc399eb17ee
maxisab <max.sabate@gmail.com>
2024-08-19 18:20:56 -0700

    created some basic components and interfaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import PostCard from '../components/PostCard';
 
 export default function Home() {
   return (
     <main>
       <h2>Home</h2>
+      <PostCard title="some title" author="Mario" />
     </main>
   );
 }
