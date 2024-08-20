6dbcdadc737987235d85dde3f405c2e477e753f1
maxisab <max.sabate@gmail.com>
2024-08-19 18:10:46 -0700

    new practice project

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <main>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus totam eaque, aperiam reiciendis minima, dolore accusantium debitis labore similique odio quas reprehenderit aspernatur quaerat itaque consectetur, illum quasi quia? Alias.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio nulla natus distinctio dolor numquam nostrum ad, praesentium quasi voluptate voluptatum vel ratione repellendus laborum aspernatur deserunt velit eligendi fugit rerum nam. Minima magnam quibusdam voluptate odio voluptatibus, molestias dolores earum minus similique nesciunt hic atque excepturi, qui asperiores nulla! Dolore necessitatibus modi, maiores vel, reprehenderit ut tempora est temporibus ab suscipit, explicabo tenetur magnam neque nesciunt error? Nihil, autem eos, corporis, quisquam quidem sunt aliquam officia eaque est inventore blanditiis deleniti nisi. Tempora rerum beatae et nesciunt, qui voluptates ipsum aliquam nam eligendi provident est illo repudiandae! Voluptates, eius enim?</p>
+    </main>
+  )
+}
